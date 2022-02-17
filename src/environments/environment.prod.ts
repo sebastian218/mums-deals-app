04f1345ce9e8894a621e17eb6d91e1104783fd76
Sebastián Rodero <sebastianrodero@Sebastians-MacBook-Air.local>
2022-02-17 14:38:33 -0300

    fix: styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
     path: 'https://kabsa.yallababy.com/api/v1/products/best-selling-products-by-subcategory',
     secretKey: '1DIPIkKeq8'
   },
-  cacheBusterHash: 'a739ddbe8e6a11ecb9090242ac120002'
+  cacheBusterHash: '5f5b6a67231f408a8380b8dfdc18dd17'
 };
